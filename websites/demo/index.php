48406c6571cf0d0a8811ec361ab7c2a1c948c76e
Alyssa <38295612+alyssabenipayo@users.noreply.github.com>
2022-09-21 15:05:34 -0700

    Add files via upload
    initial commit

---
+++ websites/demo/index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+    <style>
+        body {
+            display:grid;
+            place-items:center;
+            height: 100vh;
+            margin:0;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+
+    <?php
+
+        $name = "Dark Matter";
+        $read = false;
+
+    if ($read == true) {
+
+        $message = "You have read $name";
+
+    } else {
+
+        $message = "You have NOT read $name";
+
+    }
+
+    ?>
+
+    <h1>
+        <?php echo $message; ?>
+    </h1>
+
+
+
+</body>
+</html>
