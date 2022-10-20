0e28d8c9f20aa484d764d4b7277c9fe337a51fc7
alyssabenipayo <alymaebenipayo@gmail.com>
2022-10-19 22:01:39 -0700

    Save point for learning about die();

--- websites/dynamicWebApp/index.php
+++ websites/dynamicWebApp/index.php
@@ -1,6 +1,11 @@
 <?php
 
 //Superglobals are variables that are accessible from any script or file.
-var_dump($SERVER);
+echo "<pre>";
+var_dump($_SERVER);
+echo "</pre>";
+
+//die(); kills everything after it.
+die();
 
 require "views/index.view.php";
