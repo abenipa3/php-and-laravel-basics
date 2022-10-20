3a6fd1f36ce090f2be8bab20391fea4657bc9ebb
alyssabenipayo <alymaebenipayo@gmail.com>
2022-10-19 23:12:25 -0700

    Added [REQUEST_URI] to nav partials

--- websites/dynamicWebApp/index.php
+++ websites/dynamicWebApp/index.php
@@ -11,12 +11,4 @@ function dd($value) {
     die();
 }
 
-// if ($_SERVER['REQUEST_URI'] === '/') {
-//     echo 'bg-gray-900 text white';
-// } else {
-//     echo 'text-gray-300';
-// }
-
-echo $_SERVER['REQUEST_URI'] === '/' ? 'bg-gray-900 text white' : 'text-gray-300';
-
 require "views/index.view.php";
