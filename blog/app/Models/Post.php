ebe1ca1a545cb627dcd61758f053422d2801ff77
alyssabenipayo <alymaebenipayo@gmail.com>
2022-10-20 20:09:18 -0700

    Completed - Use Filesystem Class to Read a Directory.

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -11,9 +11,7 @@ class Post {
     {
         $files = File::files(resource_path("posts/"));
 
-        return array_map(function ($file){
-            return $file->getContents();
-        }, $files);
+        return array_map(fn($file) => $file->getContents(), $files);
 
     }
 
