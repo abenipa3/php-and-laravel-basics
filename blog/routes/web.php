fe8d0f0c062ffe7e4032a006b2aebd27313c1663
alyssabenipayo <alymaebenipayo@gmail.com>
2022-10-20 19:44:03 -0700

    Another overview on filesystem class - getContents.

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $post = Post::all();
 
-    ddd($posts[0]->getPathname());
+    ddd($posts[0]->getContents());
 
     return view('posts', [
         'posts' => $posts
