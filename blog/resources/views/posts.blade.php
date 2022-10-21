db5fa911feaeac30ef8373b6e78deca82852a98d
alyssabenipayo <alymaebenipayo@gmail.com>
2022-10-20 19:42:42 -0700

    Saving progress. DDD filesystem class to read a directory overview.

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -8,29 +8,10 @@
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
+    <?php foreach ($posts as $post): ?>
     <article>
-    <h1><a href="/posts/my-first-post">My First Post</a></h1>
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore quaerat eveniet, molestias non quae quisquam! Accusantium, eius voluptates provident aperiam architecto totam a necessitatibus eveniet, ut, placeat officia. Facere, quos.
-        </p>
-
+        <?= $post; ?>
     </article>
-
-    <article>
-    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore quaerat eveniet, molestias non quae quisquam! Accusantium, eius voluptates provident aperiam architecto totam a necessitatibus eveniet, ut, placeat officia. Facere, quos.
-        </p>
-
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore quaerat eveniet, molestias non quae quisquam! Accusantium, eius voluptates provident aperiam architecto totam a necessitatibus eveniet, ut, placeat officia. Facere, quos.
-        </p>
-
-    </article>
-
+    <?php endforeach; ?>
 </body>
 </html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,18 +1,33 @@
 <?php
+namespace App\Models;
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
-    return view('posts');
+    $post = Post::all();
+
+    ddd($posts[0]->getPathname());
+
+    return view('posts', [
+        'posts' => $posts
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug){
-    //  Find a post by its slug and pass it to a view called "post"
-    $post = Post::find($slug);
-
     return view ("post", [
-        'post' => $post
+        'post' => Post::find($slug)
     ]); 
-    
+
 })->where('post', '[A-z_\-]+');
