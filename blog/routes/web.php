fa9d482736045b2af9ef883dad56854842039200
alyssabenipayo <alymaebenipayo@gmail.com>
2022-10-20 18:30:36 -0700

    Completed Use Caching for Expensive Operation.

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,37 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('posts');
 });
 
 Route::get('posts/{post}', function ($slug){
-    $path = __DIR__."/../resources/posts/$slug.html";
 
-    if (! file_exists($path)){
+    if (! file_exists($path = __DIR__."/../resources/posts/$slug.html")){
         return redirect("/");
-        // ddd("File does not exist");
-        // abort(404);
+
     }
 
-    //PHP STORM - function() can be cleaned up with fn()
     $post = cache()->remember("posts.{$slug}", 1200, function() use ($path)
     {
         return $post = file_get_contents($path);
     });
-
-
 
     return view('post', [
         'post' => $post
