8f41eed62c3ec50957edd5585dfc5bd6860918fd
alyssabenipayo <alymaebenipayo@gmail.com>
2022-10-21 21:37:56 -0700

    Composer Package: Array_Map - Saving Progress.

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -18,35 +18,18 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
     $files = File::files(resource_path("posts"));
-    $posts = [];
 
-    // $posts = array_map(function ($file) {
-    //     $document = YamlFrontMatter::parseFile($file);
-    //     return new Post(
-    //         $document->title,
-    //         $document->excerpt,
-    //         $document->date,
-    //         $document->body(),
-    //         $document->slug,
-    //     );
-    // }, $files);
-
-    foreach ($files as $file) {
+    $posts = array_map(function ($file) {
         $document = YamlFrontMatter::parseFile($file);
-
-        // To add meta-data, add the front matter.
-        $posts[] = new Post(
+        return new Post(
             $document->title,
             $document->excerpt,
             $document->date,
             $document->body(),
             $document->slug,
-
         );
-    }
-    return view('posts', [
-        'posts' => $posts
-    ]);
+    }, $files);
+
 });
 
 Route::get('posts/{post}', function ($slug){
