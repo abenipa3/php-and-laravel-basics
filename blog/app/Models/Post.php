fc04eb8c617709760ca7a72447ac22b0f974dce7
alyssabenipayo <alymaebenipayo@gmail.com>
2022-10-21 22:07:34 -0700

    Composer Package: dd posts static::all.

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post {
 
@@ -23,25 +24,31 @@ class Post {
         $this->excerpt = $excerpt;
         $this->date = $date;
         $this->body = $body;
+        $this->slug = $slug;
 
     }
 
     public static function all()
     {
-        $files = File::files(resource_path("posts/"));
-
-        return array_map(fn($file) => $file->getContents(), $files);
+        return collect(File::files(resource_path("posts")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ));
 
     }
 
     public static function find($slug)
     {
+        //of all the blog posts, find the one with a slug that matches the one that was requested.
 
-        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
+        $posts = static::all();
 
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+        dd($posts->firstWhere('slug', $slug));
 
     }
 }
--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -12,8 +12,9 @@
     <article>
 
         <h1>
-            <a href="/posts/<?= $post->slug; ?>"></a>
-            <?= $post->title; ?>
+            <a href="/posts/<?= $post->slug; ?>">
+                <?= $post->title; ?>
+            </a>
         </h1>
 
         <div>
