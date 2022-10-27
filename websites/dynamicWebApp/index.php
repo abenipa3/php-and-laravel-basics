7c371680a2a809ad5a2034a86d0299285fad556d
Alyssa <alyssa.benipayo@nelnet.net>
2022-10-27 15:38:59 -0700

    PDO First Steps Lesson Completed.

--- websites/dynamicWebApp/index.php
+++ websites/dynamicWebApp/index.php
@@ -4,21 +4,17 @@ require 'functions.php';
 //require 'router.php';
 
 // Connect to our MySQL Database - PDO (PHP Data Objects)
+// DSN - data source name
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
 
-class Person {
-    public $name;
-    public $age;
+$pdo = new PDO($dsn);
 
-    public function breathe()
-    {
-        echo $this->name . " is breathing";
+$statement = $pdo->prepare("select * from posts");
 
-    }
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
 }
-
-$person = new Person();
-
-$person->name = 'John Doe';
-$person->age = '25';
-
-$person->breathe();
