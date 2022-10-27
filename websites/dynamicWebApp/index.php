a92100bccf0b1b0c1bc0725af4215a854a5c29c7
Alyssa <alyssa.benipayo@nelnet.net>
2022-10-27 15:09:44 -0700

    PDO First Steps: Saving Progress.

--- websites/dynamicWebApp/index.php
+++ websites/dynamicWebApp/index.php
@@ -1,4 +1,24 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+// Connect to our MySQL Database - PDO (PHP Data Objects)
+
+class Person {
+    public $name;
+    public $age;
+
+    public function breathe()
+    {
+        echo "breathing";
+
+    }
+}
+
+$person = new Person();
+
+$person->name = 'John Doe';
+$person->age = '25';
+
+dd($person->breathe());
