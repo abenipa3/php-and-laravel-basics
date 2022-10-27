197c448e71410001c98f55546594ea7a99b08cd7
Alyssa <alyssa.benipayo@nelnet.net>
2022-10-27 12:00:35 -0700

    Make Php Router: Saving Progress. Added 404 page. Index = WIP.

--- websites/dynamicWebApp/index.php
+++ websites/dynamicWebApp/index.php
@@ -4,12 +4,20 @@ require 'functions.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-if ($uri === '/'){
-    require 'controllers/index.php';
-} else if ($uri === '/about'){
-    require 'controllers/about.php';
-} else if ($uri === '/contact'){
-    require 'controllers/contact.php';
-} else if ($uri === '/mission'){
-    require 'controllers/mission.php';
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+    '/mission' => 'controllers/mission.php',
+];
+
+// If the array has a key with the given name, it exists
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+} else {
+    http_response_code(404);
+
+    require 'views/404.php';
+
+    die();
 }
